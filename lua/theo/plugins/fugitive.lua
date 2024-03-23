2553356433d2bfb2fba5047b04d97d896b3364de
Theo <theo.lettermann@gmail.com>
2024-03-23 19:04:55 +0100

    update fugitive to personal config

--- lua/theo/plugins/fugitive.lua
+++ lua/theo/plugins/fugitive.lua
@@ -3,25 +3,24 @@ return {
     config = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
-        -- TODO: figure out what this does
-        local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+        local fugitiveGroup = vim.api.nvim_create_augroup("fugitiveGroup", {})
 
         local autocmd = vim.api.nvim_create_autocmd
         autocmd("BufWinEnter", {
-            group = ThePrimeagen_Fugitive,
+            group = fugitiveGroup,
             pattern = "*",
             callback = function()
-                if vim.bo.ft ~= "fugitive" then
+                if vim.bo.filetype ~= "fugitive" then
                     return
                 end
 
-                local bufnr = vim.api.nvim_get_current_buf()
-                local opts = {buffer = bufnr, remap = false}
+                local buffer_num = vim.api.nvim_get_current_buf()
+                local opts = {buffer = buffer_num, remap = false}
+
                 vim.keymap.set("n", "<leader>p", function()
                     vim.cmd.Git('push')
                 end, opts)
 
-                -- rebase always
                 vim.keymap.set("n", "<leader>P", function()
                     vim.cmd.Git({'pull',  '--rebase'})
                 end, opts)
