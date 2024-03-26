5376c87ed027e34d6ebc490a17b046fd214eae9d
Theo <theo.lettermann@gmail.com>
2024-03-26 12:29:53 +0100

    feat: lsp, neogen and snippets setup

---
+++ lua/theo/plugins/snippets.lua
@@ -0,0 +1,25 @@
+return {
+    "L3MON4D3/LuaSnip",
+    version = "v2.*",
+    build = "make install_jsregexp",
+
+    dependencies = { },
+
+    config = function ()
+        local ls = require("luasnip")
+        ls.filetype_extend("javascript", { "jsdoc" })
+
+        require("luasnip.loaders.from_lua").lazy_load()
+
+        vim.keymap.set({"i"}, "<C-s>e", function () ls.expand() end, {silent = true})
+
+        vim.keymap.set({"i", "s"}, "<C-s>;", function () ls.jump(-1) end, {silent = true})
+        vim.keymap.set({"i", "s"}, "<C-s>,", function () ls.jump(1) end, {silent = true})
+
+        vim.keymap.set({"i", "s"}, "<C-E>", function ()
+            if ls.choice_active() then
+                ls.change_choice(1)
+            end
+        end, {silent = true})
+    end
+}
