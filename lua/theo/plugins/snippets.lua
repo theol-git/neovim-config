68d13fbe173d33c8603f59116b05fa3c81c6942d
Theo <theo.lettermann@gmail.com>
2024-03-26 12:41:54 +0100

    fix: remove dead code

--- lua/theo/plugins/snippets.lua
+++ lua/theo/plugins/snippets.lua
@@ -1,13 +1,12 @@
 return {
     "L3MON4D3/LuaSnip",
-    version = "v2.*",
-    build = "make install_jsregexp",
 
-    dependencies = { },
+    version = "v2.*",
+
+    build = "make install_jsregexp",
 
     config = function ()
         local ls = require("luasnip")
-        ls.filetype_extend("javascript", { "jsdoc" })
 
         require("luasnip.loaders.from_lua").lazy_load()
 
