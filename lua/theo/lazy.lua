8ab79c2fc676ac67a985ad5a9c5daf52068a5bd5
Theo <theo.lettermann@gmail.com>
2024-03-26 12:39:02 +0100

    fix: remove commented code

--- lua/theo/lazy.lua
+++ lua/theo/lazy.lua
@@ -14,30 +14,3 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     spec = "theo.plugins",
 })
---[[
-require("lazy").setup({
-	use ("ThePrimeagen/harpoon")
-})
-
-	use {
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v3.x",
-		requires = {
-			-- LSP Support
-			{"neovim/nvim-lspconfig"},
-			{"williamboman/mason.nvim"},
-			{"williamboman/mason-lspconfig.nvim"},
-
-			-- Autocompletion
-			{"hrsh7th/nvim-cmp"},
-			{"hrsh7th/cmp-nvim-lsp"},
-			{"hrsh7th/cmp-buffer"},
-			{"hrsh7th/cmp-path"},
-			{"saadparwaiz1/cmp_luasnip"},
-			{"saadparwaiz1/cmp_luasnip"},
-			{"hrsh7th/cmp-nvim-lua"},
-			{"L3MON4D3/LuaSnip"},
-			{"rafamadriz/friendly-snippets"},
-		}
-	}
-]]--
