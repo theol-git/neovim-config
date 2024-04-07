cd0a481ae7e06e4808018fcb0d2aaae209fccca1
Theo <theo.lettermann@gmail.com>
2024-04-07 23:43:12 +0200

    feat: add keymaps

--- lua/theo/remap.lua
+++ lua/theo/remap.lua
@@ -3,3 +3,9 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<leader>x", ":!chmod +x %<CR>")
+
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
