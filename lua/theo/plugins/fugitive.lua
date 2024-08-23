3944970319a81036ede8d87839f9ee1cdb3dd0ef
t.lettermann <t.lettermann@criteo.com>
2024-08-23 11:08:39 +0200

    chore: add git review keymap to fugitive

--- lua/theo/plugins/fugitive.lua
+++ lua/theo/plugins/fugitive.lua
@@ -25,6 +25,10 @@ return {
 					vim.cmd.Git("pull  --rebase")
 				end, opts)
 
+				vim.keymap.set("n", "<leader>r", function()
+					vim.cmd.Git("review")
+				end, opts)
+
 				-- NOTE: It allows me to easily set the branch i am pushing and any tracking
 				-- needed if i did not set the branch up correctly
 				vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
