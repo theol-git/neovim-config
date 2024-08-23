9bbef15c931d3474aa0ba0ad7e09af62b8c0e65e
t.lettermann <t.lettermann@criteo.com>
2024-08-23 11:09:51 +0200

    chore: try and get scala metals working properly

--- lua/theo/plugins/scala-metals.lua
+++ lua/theo/plugins/scala-metals.lua
@@ -10,8 +10,20 @@ return {
         metals_config.settings = {
             serverVersion = "latest.snapshot",
         }
+
+        metals_config.init_options.statusBarProvider = "off"
+
         metals_config.on_attach = function(client, bufnr)
-            -- your on_attach function
+            vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end)
+            vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end)
+            vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
+            vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end)
+            vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end)
+            vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end)
+            vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end)
+            vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end)
+            vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end)
+            vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end)
         end
 
         return metals_config
